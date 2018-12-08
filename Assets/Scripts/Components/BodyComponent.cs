5d1007798ce85f80e32e9bb2d78fe8dd5bb9f3f7
Milos Kruskonja <milosk@nordeus.com>
2018-12-08 11:30:54 +0100

    Duvaj ga sale. Add initial empty Body and Hand Components.

---
+++ Assets/Scripts/Components/BodyComponent.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Unity.Entities;
+
+[Serializable]
+public struct Body : IComponentData {
+
+}
+
+[UnityEngine.DisallowMultipleComponent]
+public class BodyComponent : ComponentDataWrapper<Body> { }
---
+++ Assets/Scripts/Components/HandComponent.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Unity.Entities;
+
+[Serializable]
+public struct Hand : IComponentData {
+
+}
+
+[UnityEngine.DisallowMultipleComponent]
+public class HandComponent : ComponentDataWrapper<Hand> { }
