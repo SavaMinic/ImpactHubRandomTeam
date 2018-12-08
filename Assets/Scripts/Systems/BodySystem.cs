84b8be24772b7ad03e45e24f8473373e1630a8e4
Milos Kruskonja <milosk@nordeus.com>
2018-12-08 11:59:58 +0100

    Add BodySystem.

---
+++ Assets/Scripts/Systems/BodySystem.cs
@@ -0,0 +1,25 @@
+﻿using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Burst;
+using Unity.Transforms;
+using UnityEngine;
+using RandomName.Wave;
+
+public class BodySystem : JobComponentSystem {
+
+    [BurstCompile]
+    struct BodyJob : IJobProcessComponentData<Position, Body, WavingFan> {
+
+        public void Execute(ref Position position, [ReadOnly]ref Body body, [ReadOnly]ref WavingFan wavingFan) {
+            position.Value.y = body.InitPosition.y + wavingFan.Value;
+        }
+
+    }
+
+    protected override JobHandle OnUpdate(JobHandle inputDeps) {
+        var job = new BodyJob() {};
+        return job.Schedule(this, inputDeps);
+    }
+
+}
